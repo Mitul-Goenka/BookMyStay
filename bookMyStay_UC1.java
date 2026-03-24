e2b72e5a49435e05e3612d349175b06c1ba1da53
dhananjaykumar077 <dhananjaykumar89059@gmail.com>
2026-03-24 14:19:27 +0530

    Initial commit

---
+++ bookMyStay_UC1.java
@@ -0,0 +1,14 @@
+public class bookMyStay_UC1 {
+    public static void main(String[] args){
+        String appName = "Book My Stay";
+        String version = "v1.0";
+
+
+        System.out.println("==================================");
+        System.out.println(" Welcome to " + appName);
+        System.out.println(" Version: " + version);
+        System.out.println("==================================");
+        System.out.println("Thank you for choosing our service!");
+    }
+
+}
